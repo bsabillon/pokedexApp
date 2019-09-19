017ff9460956c82799fd5c4c2eee57f86dfc08bf
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-09-19 08:53:45 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { PokemonComponent } from './components/pokemons/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
